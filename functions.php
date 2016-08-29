5c028d616fbf1f4a1e957788a7d27dce58175081
John Cosio <mail@johncosio.com>
2016-08-29 11:01:59 +0800

    Version: 1.0.0 -Initial Commit
    Signed-off-by: John Cosio <mail@johncosio.com>

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+// Include Beans. Do not remove the line below.
+require_once( get_template_directory() . '/lib/init.php' );
+
+// Remove this action and callback function if you are not adding CSS in the style.css file.
+add_action( 'wp_enqueue_scripts', 'beans_child_enqueue_assets' );
+
+function beans_child_enqueue_assets() {
+
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
+
+}
